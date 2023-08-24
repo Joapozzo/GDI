cc1db7e1b47cc2a25e8099d42b61ca4921b583e4
root <mgiraudo@ies21.edu.ar>
2023-08-24 13:53:12 -0300

    .

---
+++ GDI/frmArkanoid.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace GDI
+{
+    partial class frmArkanoid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contenedor = new System.Windows.Forms.PictureBox();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.temporizador = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.contenedor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contenedor
+            // 
+            this.contenedor.Location = new System.Drawing.Point(-2, -2);
+            this.contenedor.Name = "contenedor";
+            this.contenedor.Size = new System.Drawing.Size(611, 457);
+            this.contenedor.TabIndex = 0;
+            this.contenedor.TabStop = false;
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(208, 146);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(204, 133);
+            this.btnJugar.TabIndex = 1;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // temporizador
+            // 
+            this.temporizador.Tick += new System.EventHandler(this.temporizador_Tick);
+            // 
+            // frmArkanoid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 450);
+            this.Controls.Add(this.btnJugar);
+            this.Controls.Add(this.contenedor);
+            this.Name = "frmArkanoid";
+            this.Text = "frmArkanoid";
+            this.Load += new System.EventHandler(this.frmArkanoid_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmArkanoid_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmArkanoid_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.contenedor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox contenedor;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Timer temporizador;
+    }
+}
---
+++ GDI/frmArkanoid.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDI
+{
+    public partial class frmArkanoid : Form
+    {   
+        Pen lapiz = new Pen(Color.Blue);
+        Graphics lienzo;
+        public frmArkanoid()
+        {
+            InitializeComponent();
+        }
+
+        private void frmArkanoid_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnJugar.Hide();
+            lienzo = contenedor.CreateGraphics();
+            lienzo.DrawRectangle(lapiz, 230, 320, 150, 70);
+            temporizador.Enabled = true;
+        }
+
+        int ejeX, ejeY = 0;
+        Random aleatorioX = new Random();
+        Random aleatorioY = new Random();
+
+        private void frmArkanoid_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void frmArkanoid_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyChar = Left)
+            //{
+            //    MessageBox.Show("Test");
+            //}
+        }
+
+        private void temporizador_Tick(object sender, EventArgs e)
+        {
+            ejeX = aleatorioX.Next(0, 500);
+            ejeX = aleatorioY.Next(0, 500);
+
+            lienzo.DrawRectangle(lapiz, ejeX, ejeY, 150, 700);
+
+        }
+    }
+}
